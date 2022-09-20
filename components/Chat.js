131481bcd181607719f2e1d89970853bbef8ca89
smmartin1 <smarie333m@gmail.com>
2022-09-19 22:14:11 -0400

    Installed and implemented Firebase

--- components/Chat.js
+++ components/Chat.js
@@ -2,46 +2,92 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
 export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
-      text: '',
-      messages: [],
+      uid: 0,
+      messages: []
     }
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyAnGpBYfhOP0cHsLodtrXnDGS8TRl8LeDo",
+      authDomain: "chat-app-44562.firebaseapp.com",
+      projectId: "chat-app-44562",
+      storageBucket: "chat-app-44562.appspot.com",
+      messagingSenderId: "833277919354",
+      appId: "1:833277919354:web:fc66de7175f501a11efbc3",
+      measurementId: "G-LY3X7KYB7Y"
+    };
+
+    if (!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    this.referenceChatMessages = firebase.firestore().collection("messages");
   }
 
-  componentDidMount(){
+  componentDidMount() {
     //Name choosen
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
 
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+
+      this.setState({
+        uid: user.uid,
+        messages: []
+      });
+    });
+
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+    this.unsubscribe = this.referenceChatMessages.orderBy("createdAt", "desc").onSnapshot(this.onCollectionUpdate);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    querySnapshot.forEach((doc) => {
+        let data = doc.data();
+        messages.push({
+          _id: data._id,
+          text: data.text,
+          createdAt: data.createdAt.toDate(),
+          user: data.user,
+        });
+    });
+
     this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello, ' + name,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: name + ' has entered the chat',
-          createdAt: new Date(),
-          system: true
-        }
-      ],
-    })
+      messages
+    });
+  }
+
+  addMessage() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user
+    });
   }
 
   onSend(messages = []) {
    this.setState(previousState => ({
      messages: GiftedChat.append(previousState.messages, messages),
-   }))
+   }), () => {
+     this.addMessage();
+   });
   }
 
   renderBubble(props) {
@@ -67,7 +113,9 @@ export default class Chat extends React.Component {
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           user={{
-            _id: 1,
+            _id: this.state.uid,
+            avatar: 'https://placeimg.com/140/140/any',
+            name: this.props.route.params.name
           }}
         />
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
@@ -75,24 +123,3 @@ export default class Chat extends React.Component {
     )
   }
 }
-
-/*
-const styles = StyleSheet.create({
-  message: {
-    height: 40,
-    width: '88%',
-    padding: 10,
-    borderColor: 'gray',
-    borderWidth: 1,
-    backgroundColor: '#E6E6E6'
-  },
-  button: {
-    backgroundColor: '#CCCCCC',
-    height: 50,
-    width: '88%',
-    marginTop: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
-*/
